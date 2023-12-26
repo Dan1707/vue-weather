05cbbd7ff47ec4ff603564988ec574b55b37f05c
Dan1707 <132587001+Dan1707@users.noreply.github.com>
2023-12-26 15:38:45 +0200

    Add files via upload

---
+++ src/components/Vue-popup.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="popup__container">
+    <div class="popup__exit" @click="$emit('close')">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        class="popup__exit_btn"
+        viewBox="0 0 50 50"
+      >
+        <path
+          d="M 25 2 C 12.309534 2 2 12.309534 2 25 C 2 37.690466 12.309534 48 25 48 C 37.690466 48 48 37.690466 48 25 C 48 12.309534 37.690466 2 25 2 z M 25 4 C 36.609534 4 46 13.390466 46 25 C 46 36.609534 36.609534 46 25 46 C 13.390466 46 4 36.609534 4 25 C 4 13.390466 13.390466 4 25 4 z M 32.990234 15.986328 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.990234 15.986328 z"
+        ></path>
+      </svg>
+    </div>
+    <h2 class="popup__title">Weeekly weather forecast</h2>
+    <div class="popup__day_wrapper">
+      <div
+        class="popup__day_item"
+        v-for="day in weatherItemForecast"
+        :key="day.dayName"
+      >
+        <div class="popup__day_item-start">
+          <h3 class="popup__day_item-date">{{ day.dayName }}</h3>
+          <div class="popup__day_item-img-wrapper">
+            <img :src="day.weatherIcon" class="popup__day_item-img" />
+          </div>
+          <h4 class="popup__day_item-suptitle">
+            {{ day.weatherIconText }}
+          </h4>
+        </div>
+        <p class="popup__day_item-maxtemp">Max temp: {{ day.maxTemp }}</p>
+        <p class="popup__day_item-mintemp">Min temp: {{ day.minTemp }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    weatherItemForecast: Array,
+  },
+  emits: ["close"],
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/weather-card.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="weather__item">
+    <h3 class="weather__item_name">{{ weather.cityName }}</h3>
+    <div class="weather__item_ico-wrapper">
+      <img :src="weather.weatherIcon" alt="Ico" class="weather__item_ico" />
+    </div>
+    <h2 class="weather__item_deg">{{ weather.temperature }}C</h2>
+
+    <h3 class="weather__item_outside">{{ weather.condition }}</h3>
+    <p class="weather__item_feelslike">Feels like: {{ weather.feelsLike }}C</p>
+    <button
+      class="weather__item_delete-btn"
+      @click.stop="deleteWeather(weather)"
+    >
+      Delete
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  props: {
+    weather: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.weather {
+  &__item-wrapper {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat (auto, 250px);
+    justify-content: space-between;
+    align-items: center;
+    max-width: 800px;
+    gap: 20px;
+    margin: 0 auto;
+    margin-top: 50px;
+  }
+  &__item {
+    padding: 20px 20px 50px 20px;
+    border-radius: 15px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #2770a1;
+    color: #ffffff;
+    text-align: center;
+    position: relative;
+    box-shadow: 0px 0px 40px -7px rgba(0, 0, 0, 0.75);
+    overflow: hidden;
+    cursor: pointer;
+    &:hover {
+      .weather__item_delete-btn {
+        opacity: 1;
+      }
+    }
+  }
+}
+</style>
